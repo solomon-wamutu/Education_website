f8ab7dbac784a3116a332e3afd857d5c4a2d7c41
solomon-wamutu <solomonwamutu16@gmail.com>
2023-07-07 01:10:03 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <nav>
+        <div class="container nav_container">
+            <a href="index.php">
+                <h4>Lilac</h4>
+            </a>
+        </div>
+        <ul class="nav_menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="courses.php">Courses</a></li>
+            <li><a href="content.php">Contacts</a></li>
+        </ul>
+        <button id="open-menu-btn"></button>
+        <button id="close-menu-btn"></button>
+    </nav>
+</body>
+</html>
